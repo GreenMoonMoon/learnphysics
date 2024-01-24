698a4c791b14b58dbcc7349bc2c4981aaf22e224
moonmoon <josue.boisvert@gmail.com>
2024-01-24 09:21:20 -0500

    adding cglm as the math library

--- src/rigidbody.h
+++ src/rigidbody.h
@@ -5,4 +5,17 @@
 #ifndef LEARNPHYSICS_RIGIDBODY_H
 #define LEARNPHYSICS_RIGIDBODY_H
 
+#include "cglm/cglm.h"
+
+typedef struct Rigidbody {
+    vec3 velocity;
+    mat4 transform;
+} Rigidbody;
+
+void integrate_velocity(Rigidbody *rigidbody, float delta_time) {
+    vec3 scaled_velocity;
+    glm_vec3_scale(rigidbody->velocity, delta_time, scaled_velocity);
+    rigidbody->transform = glm_translate(rigidbody->transform, scaled_velocity);
+}
+
 #endif //LEARNPHYSICS_RIGIDBODY_H
