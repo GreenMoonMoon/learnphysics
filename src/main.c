3ed5cd2cc6c3b6b793365ee30126d0fab6afdd1b
moonmoon <josue.boisvert@gmail.com>
2024-01-23 22:11:41 -0500

    setup basic scene

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include "raylib.h"
+#include "raymath.h"
 
 #include "rigidbody.h"
 
+typedef struct Entity {
+    Mesh mesh;
+    Material material;
+    Matrix transform;
+} Entity;
+Entity entities[2];
 
 int main() {
     InitWindow(1080, 720, "Learn Physics");
@@ -16,6 +23,21 @@ int main() {
             .projection = CAMERA_PERSPECTIVE
     };
 
+    // Load entities
+    entities[0] = (Entity){
+        .mesh = GenMeshCube(1.0f, 1.0f, 1.0f),
+        .material = LoadMaterialDefault(),
+        .transform = MatrixTranslate(0.0f, 2.0f, 0.0f)
+    };
+    entities[0].material.maps[MATERIAL_MAP_DIFFUSE].color = BLUE;
+
+    entities[1] = (Entity){
+            .mesh = GenMeshCube(1.0f, 1.0f, 1.0f),
+            .material = LoadMaterialDefault(),
+            .transform = MatrixTranslate(0.5f, 4.0f, 0.5f)
+    };
+    entities[1].material.maps[MATERIAL_MAP_DIFFUSE].color = RED;
+
     while(!WindowShouldClose()) {
         // Update
 //        UpdateCamera(&camera, CAMERA_FREE);
@@ -26,6 +48,11 @@ int main() {
 
         BeginMode3D(camera);
         DrawGrid(16, 1);
+
+        for(int i = 0; i < 2; i++) {
+            DrawMesh(entities[i].mesh, entities[i].material, entities[i].transform);
+        }
+
         EndMode3D();
 
         EndDrawing();
